d7aa1f5b22079974110eee69bee1452eba8b2c9b
carnivorecoder <46478769+carnivorecoder@users.noreply.github.com>
2019-01-08 01:36:58 -0700

    Update wintwo.py

--- wintwo.py
+++ wintwo.py
@@ -1,4 +1,11 @@
 def weight_to_height():
+    """
+    Inputs body measurements and gender, and outputs whether you are at a healthy ratio or not.
+    :param gender: int.
+    :param height: int.
+    :param weight: int.
+    :print: 
+    """
     gendercheck = int(input("What is your birth gender? Enter an even number for female, or an odd number for male."))
     if gendercheck % 2 == 0:
         gender = 0
@@ -9,12 +16,11 @@ def weight_to_height():
     ratio = waist / height
     if gender == 0:
         if ratio <= 0.4919:
-            print("Looks like your waist to height ratio is great, young lady!")
+            print("Looks like your waist to height ratio of "+str(ratio)+" is great, young lady!")
         else:
-            print("Looks like you currently have an increased risk of insulin resistance, type 2 diabetes and heart disease!")
+            print("Looks like you currently have an increased risk of insulin resistance, type 2 diabetes and heart disease at "+str(ratio)+"! Aim to get below .4920.")
     else:
         if ratio <= 0.5359:
-            print("Looks like your waist to height ratio is great, young man!")
+            print("Looks like your waist to height ratio of "+str(ratio)+" is great, young man!")
         else:
-            print("Looks like you currently have an increased risk of insulin resistance, type 2 diabetes and heart disease!")
-
+            print("Looks like you currently have an increased risk of insulin resistance, type 2 diabetes and heart disease at "+str(ratio)+"! Aim to get below .5260.")
